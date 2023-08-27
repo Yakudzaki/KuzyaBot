ef6bc7d222f4b23dab25036aaa5a6d4894b1eec1
AleDerXan <143110837+AleDerXan@users.noreply.github.com>
2023-08-27 19:55:51 +0300

    Clans update

--- handlers/user/clans/clan_list.py
+++ handlers/user/clans/clan_list.py
@@ -10,6 +10,6 @@ async def clans_top_list(message: types.Message):
 
     for i, clan in enumerate(clans_top):
         players_count = get_clans_count_players(clan[0])
-        msg_text += f'{i + 1}. {clan[1]} | {players_count} уч. | Казна: {clan[4]:.} монет.\n'
+        msg_text += f'{i + 1}. {clan[1]} | {players_count} уч. | Казна: {clan[4]} монет.\n'
 
     await message.answer(msg_text)
