14abdbd03c0bb068f792c510d770d5ab74cc8e5d
x-four-x <grigorevkirill20@gmail.com>
2023-08-27 19:30:40 +0300

    Add count player in clans top

--- handlers/user/clans/clan_list.py
+++ handlers/user/clans/clan_list.py
@@ -1,14 +1,15 @@
 from aiogram import types
 from loader import dp
-from utils.db.db_utils_clans import get_top_clans
+from utils.db.db_utils_clans import get_top_clans, get_clans_count_players
 
 
 @dp.message_handler(commands=['кланы', 'список кланов', 'топ кланов', 'clans'], commands_prefix='!?./')
 async def clans_top_list(message: types.Message):
     clans_top = get_top_clans()
-    msg_text = 'Топ 10 кланов:\n\n'
+    msg_text = f'Топ {len(clans_top)} кланов:\n\n'
 
     for i, clan in enumerate(clans_top):
-        msg_text += f'{i}. {clan[1]} | Казна: {clan[4]} монет.\n'
+        players_count = get_clans_count_players(clan[0])
+        msg_text += f'{i + 1}. {clan[1]} | {players_count} уч. | Казна: {clan[4]:.} монет.\n'
 
     await message.answer(msg_text)
--- utils/db/db_utils_clans.py
+++ utils/db/db_utils_clans.py
@@ -1,6 +1,5 @@
 import sqlite3
-#from app import db_file as path
-path = "C:/Users/Sveta/OneDrive/Рабочий стол/Project/KuzyaBot/utils/db/kuzya.db"
+from app import db_file as path
 
 connect = sqlite3.connect(path, timeout=10)
 cursor = connect.cursor()
@@ -15,3 +14,6 @@ def get_top_clans(limit: int = 10):
     cursor.execute(f'SELECT * FROM clans ORDER BY balance DESC LIMIT ?;', (limit, ))
     return cursor.fetchall()
 
+def get_clans_count_players(clan_id: int) -> int:
+    cursor.execute('SELECT COUNT(*) FROM users WHERE clan_id = ?;', (clan_id, ))
+    return cursor.fetchone()[0]
