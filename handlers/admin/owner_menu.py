61d3f19b054f1f1d79ac80965b3c7217a9df115d
Yakudza <123520462+Yakudzaki@users.noreply.github.com>
2023-08-31 04:18:59 +0600

    Update owner_menu.py

--- handlers/admin/owner_menu.py
+++ handlers/admin/owner_menu.py
@@ -1,1 +1,17 @@
+from aiogram import types
+from loader import dp, bot
+from settings import botovod_id, legal_chats
+from keyboards.inline.dev_kb import dev_btn
 
+
+
+@dp.message_handler(commands=["!дев", "!dev"], private=True)
+async def dev_handler(message: types.Message):
+	user = message.from_user.id
+	
+	if user not in botovod_id:
+		await message.answer("Чёрт! Ты меня взломал :(")
+		return
+	
+	else:
+		await message.answer("Добро пожаловать в меню разработчика", reply_markup=dev_btn)
