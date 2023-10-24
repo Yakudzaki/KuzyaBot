d6cf5ce38ca00ab7d5f83b7858796b5c2b99753e
Yakudza <123520462+Yakudzaki@users.noreply.github.com>
2023-10-25 02:29:14 +0600

    Create anime.py

---
+++ handlers/user/anime.py
@@ -0,0 +1,19 @@
+import requests
+from loader import dp, bot
+from aiogram import types
+
+
+# Обработчик команды
+@dp.message_handler(commands=['аниме', 'тян'], commands_prefix="/!.")
+async def send_image(message: types.Message):
+    url = "https://pic.re/image"
+    response = requests.post(url)
+    data = response.json()
+    
+    # Получение данных о фотографии
+    file_url = data["file_url"]
+    source = data["source"]
+    author = data["author"]
+    
+    # Отправка фотографии и описания
+    await bot.send_photo(message.chat.id, file_url, caption=f"Источник: {source}\nАвтор: {author}")
