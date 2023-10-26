73da4d0ef3fe8388bd5ea0acc003f3d4a340b9a1
Yakudza <123520462+Yakudzaki@users.noreply.github.com>
2023-10-27 04:47:34 +0600

    Create tiktok.py

---
+++ handlers/user/tiktok.py
@@ -0,0 +1,34 @@
+import re
+import requests
+from aiogram import types
+from loader import bot, dp
+
+
+async def get_video(link, chat_id):
+	url = "https://tiktok-video-no-watermark2.p.rapidapi.com/"
+
+	payload = {
+		"url": link,
+		"hd": "1"
+	}
+	headers = {
+		"content-type": "application/x-www-form-urlencoded",
+		"X-RapidAPI-Key": "b1b83ddcf4msheb310e444c2450ep1ebd82jsnf111d01eea9f",
+		"X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com"
+	}
+
+	response = requests.post(url, data=payload, headers=headers)
+
+	return response.json()["data"]["hdplay"]
+
+
+@dp.message_handler()
+async def send_video(message: types.Message):
+    if re.compile('https://[a-zA-Z]+.tiktok.com/').match(message.text):
+        try:
+        	video_link = await get_video(message.text, message.chat.id)
+        	await bot.send_chat_action(message.chat.id, 'upload_video')
+        	await message.reply_video(video_link, caption=f'✅ Скачано с помощью {BOT_USERNAME}\n\n')
+        except:
+        	await message.reply("😢 Не удалось скачать видео!")
+        	await bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAJSC2U65o9xJhSVFgbCctFBk7yqLwL4AAKjOQACjz_QSTwjn1gib7t0MAQ")
