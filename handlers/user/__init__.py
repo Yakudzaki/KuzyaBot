b3f2dc26d5625fc85af0a8d8991aedb6a7daf675
AleDerXan <143110837+AleDerXan@users.noreply.github.com>
2023-10-28 20:00:25 +0300

    Upd

--- handlers/user/__init__.py
+++ handlers/user/__init__.py
@@ -17,7 +17,7 @@ from .weather import dp
 from .wiki import dp
 from .anime import dp
 from .horoscope import dp
-from .tiktok import dp
+# from .tiktok import dp
 from .easters import dp
 
 __all__ = ['dp']
