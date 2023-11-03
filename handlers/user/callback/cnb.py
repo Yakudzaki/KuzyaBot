280519ff293657ed4c5ffd7d6e794a31ecf63f51
AleDerXan <143110837+AleDerXan@users.noreply.github.com>
2023-11-03 20:31:17 +0300

    Upd

--- handlers/user/callback/cnb.py
+++ handlers/user/callback/cnb.py
@@ -17,29 +17,39 @@ async def process_callback_yes(callback: types.CallbackQuery):
     rand = random.choice(["🗿Камень", "✂️Ножницы", "📄Бумагу"])
     # await bot.delete_message(callback.message.chat.id, callback.message.message_id)
     user = get_user(callback.from_user.id)
+    
     role = ['👤', '👨', '👩', '👾', '😿']
     cnb_em = f"{role[user[4]]}| "
     
+    role_ch = ['выбрали', 'выбрал', 'выбрала', 'выбрало', 'выбираешь']
+    cnb_ch = role_ch[user[4]]
+    
+    role_win = ['победили', 'победил', 'победила', 'победило', 'побеждаешь']
+    cnb_win = role_win[user[4]]
+    
+    role_me = ['Вы', 'Ты', 'Ты', 'Ты', 'Ты']
+    cnb_me = role_me[user[4]]
+    
     if callback.data == '1':
-        msg1 = "🤖| Я выбрал " + rand + f"\n{cnb_em}А <a href='tg://user?id={callback.from_user.id}'>ты</a> выбрал 🗿Камень"
+        msg1 = "🤖| Я выбрал " + rand + f"\n{cnb_em}А <a href='tg://user?id={callback.from_user.id}'>{cnb_me.lower()}</a> {cnb_ch} 🗿Камень"
         if rand == '🗿Камень':
             msg2 = "У нас ничья🤝"
         elif rand == '✂️Ножницы':
-            msg2 = f"<a href='tg://user?id={callback.from_user.id}'>Ты</a> выиграл🥇"
+            msg2 = f"<a href='tg://user?id={callback.from_user.id}'>{cnb_me}</a> {cnb_win}🥇"
         else:
             msg2 = "Я победил🥇"
     elif callback.data == '2':
-        msg1 = "🤖| Я выбрал " + rand + f"\n{cnb_em}А <a href='tg://user?id={callback.from_user.id}'>ты</a> выбрал ✂️Ножницы"
+        msg1 = "🤖| Я выбрал " + rand + f"\n{cnb_em}А <a href='tg://user?id={callback.from_user.id}'>{cnb_me.lower()}</a> {cnb_ch} ✂️Ножницы"
         if rand == '🗿Камень':
             msg2 = "Я победил🥇"
         elif rand == '✂️Ножницы':
             msg2 = "У нас ничья🤝"
         else:
-            msg2 = f"<a href='tg://user?id={callback.from_user.id}'>Ты</a> победил🥇"
+            msg2 = f"<a href='tg://user?id={callback.from_user.id}'>{cnb_me}</a> {cnb_win}🥇"
     elif callback.data == '3':
-        msg1 = "🤖| Я выбрал " + rand + f"\n{cnb_em}А <a href='tg://user?id={callback.from_user.id}'>ты</a> выбрал 📄Бумагу"
+        msg1 = "🤖| Я выбрал " + rand + f"\n{cnb_em}А <a href='tg://user?id={callback.from_user.id}'>{cnb_me.lower()}</a> {cnb_ch} 📄Бумагу"
         if rand == '🗿Камень':
-            msg2 = f"<a href='tg://user?id={callback.from_user.id}'>Ты</a> победил🥇"
+            msg2 = f"<a href='tg://user?id={callback.from_user.id}'>{cnb_me}</a> {cnb_win}🥇"
         elif rand == '✂️Ножницы':
             msg2 = "Я победил🥇"
         else:
