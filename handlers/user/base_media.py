1238d122a68fef0bc51be81df7e038282b53f4ae
AleDerXan <143110837+AleDerXan@users.noreply.github.com>
2023-11-05 21:25:53 +0300

    Upd

--- handlers/user/base_media.py
+++ handlers/user/base_media.py
@@ -503,6 +503,8 @@ async def final_dice(message: types.Message):
         
     if check_user(user.id):
         return
+    if message.from_user.id == 777000 and message.sender_chat and message.forward_from_chat and message.is_automatic_forward:
+        return
     
     users = get_user(user.id)
     if users[11] < 100:
@@ -566,17 +568,20 @@ async def final_dice(message: types.Message):
 
             if message.forward_from_chat:
                 await kuzya_wait(5)
-                wins = -100
+                wins = -1000
+                add_kuzir(user.id, wins)
                 await message.reply(f"❌| Читер теряет {abs(wins)} кузиров!")
                 return
             if message.forward_from:
                 await kuzya_wait(5)
-                wins = -100
+                wins = -1000
+                add_kuzir(user.id, wins)
                 await message.reply(f"❌| Читер теряет {abs(wins)} кузиров!")
                 return
             if message.forward_sender_name:
                 await kuzya_wait(5)
-                wins = -100
+                wins = -1000
+                add_kuzir(user.id, wins)
                 await message.reply(f"❌| Читер теряет {abs(wins)} кузиров!")
                 return
 
