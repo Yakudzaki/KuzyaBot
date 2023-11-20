8e4ce4e20215a11c487e794fb2154c645c1e8c82
AleDerXan <143110837+AleDerXan@users.noreply.github.com>
2023-11-20 21:19:03 +0300

    Upd

--- handlers/user/openai.py
+++ handlers/user/openai.py
@@ -56,15 +56,19 @@ async def chatgpt(message: types.Message):
     await bot.send_chat_action(message.chat.id, types.ChatActions.TYPING)
 
     response = await chatgptg4f(promt, user, g4f.models.gpt_4)
-    if f"{response}" == "" or "您的免费额度不够使用这个模型啦，请点击右上角登录继续使用" in f"{response}":
+    if f"{response}" == "" or "您的免费额度不够使用这个模型啦，请点击右上角登录继续使用" in f"{response}" or "请求失败啦" in f"{response}":
         if "您的免费额度不够使用这个模型啦，请点击右上角登录继续使用" in f"{response}":
             print("Китайская  квота!")
         
+        print("Переходим на ГПТ 3.5!")
         response = await chatgptg4f(promt, user, "gpt-3.5-turbo")
         if f"{response}" == "" or "您的免费额度不够使用这个模型啦，请点击右上角登录继续使用" in f"{response}":
             if "您的免费额度不够使用这个模型啦，请点击右上角登录继续使用" in f"{response}":
                 print("Китайская  квота!")
-            response = "Извините, но я не могу помочь вам с этим запросом."
+            
+    if "请求失败啦" in f"{response}":
+        print("Глюк китайской нейронки!")
+        response = "Извините, но я не могу помочь вам с этим запросом."
     
     await bot.send_chat_action(message.chat.id, types.ChatActions.TYPING)
     await message.reply(f"{response}\n\nКузяGpt", disable_web_page_preview=True, parse_mode='Markdown')
