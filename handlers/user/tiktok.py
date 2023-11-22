5389fb08a9287ed49d714916f186159b7abefc5f
Yakudza <123520462+Yakudzaki@users.noreply.github.com>
2023-11-22 17:52:39 +0600

    Update tiktok.py
    Оптимизация кода

--- handlers/user/tiktok.py
+++ handlers/user/tiktok.py
@@ -1,8 +1,9 @@
 import re
-import requests
+import aiohttp
 from aiogram import types
 from loader import bot, dp
 from settings import botik_id
+
 
 async def get_video(link, chat_id):
     url = "https://tiktok-video-no-watermark2.p.rapidapi.com/"
@@ -10,19 +11,19 @@ async def get_video(link, chat_id):
     payload = {
         "url": link,
         "hd": "1"
-	}
+    }
     headers = {
         "content-type": "application/x-www-form-urlencoded",
         "X-RapidAPI-Key": "b1b83ddcf4msheb310e444c2450ep1ebd82jsnf111d01eea9f",
         "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com"
-	}
+    }
 
-    response = requests.post(url, data=payload, headers=headers)
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=payload, headers=headers) as response:
+            data = await response.json()
+            return data["data"]["hdplay"]
 
-    return response.json()["data"]["hdplay"]
 
-
-# @dp.message_handler()
 async def send_video(message: types.Message):
     if re.compile('https://[a-zA-Z]+.tiktok.com/').match(message.text):
         try:
@@ -31,6 +32,5 @@ async def send_video(message: types.Message):
             await message.reply_video(video_link, caption=f"✅ Скачано с помощью <a href='tg://user?id={botik_id}'>Кузи</a>\n\n")
         except:
             await message.reply("😢 Не удалось скачать видео!")
-            await bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAJSC2U65o9xJhSVFgbCctFBk7yqLwL4AAKjOQACjz_QSTwjn1gib7t0MAQ")
         return 1
     return
