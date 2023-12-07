f892c22a82d86fb1edfe004257d7ae3e83ee218e
Yakudza <123520462+Yakudzaki@users.noreply.github.com>
2023-12-08 01:33:58 +0600

    update app.py

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
-server_dir = "C:/Games/Soft/KuzyaBot"
-db_file = "C:/Games/Soft/KuzyaBot/utils/db/kuzya.db"
-
 import sys
-sys.path.insert(1, server_dir)
-
 import logging
-logging.basicConfig(
-    level=logging.INFO, 
-    filename="logging0.log",
-    filemode="a",
-    format="%(asctime)s %(levelname)s %(message)s"
-)
 
 import handlers
 import utils
@@ -22,11 +11,23 @@ from loguru import logger
 
 from utils.notify_admins import on_startup
 
+server_dir = "C:/Games/Soft/KuzyaBot"
+db_file = "C:/Games/Soft/KuzyaBot/utils/db/kuzya.db"
+
+sys.path.insert(1, server_dir)
+
+logging.basicConfig(
+    level=logging.INFO, 
+    filename="logging0.log",
+    filemode="a",
+    format="%(asctime)s %(levelname)s %(message)s"
+)
+
+
+
 if __name__ == '__main__':
     logging.info('Bot is starting!')
     logger.info('Bot is starting!')
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
     logging.info('Bot shuts dowm!')
     logger.info('Bot shuts dowm!')
-    
-    
