cacd940d3d2887d608fd6318f620439f95bf81de
AleDerXan <143110837+AleDerXan@users.noreply.github.com>
2024-03-06 17:39:12 +0300

    Upd

--- loader.py
+++ loader.py
@@ -5,7 +5,7 @@ from data import config
 import asyncio
 
 
-# PROXY_URL = 'socks5://127.0.0.1:9050'  #адрес локального тор (у меня запущен как служба)
+PROXY_URL = 'socks5://127.0.0.1:9050'  #адрес локального тор (у меня запущен как служба)
 storage = MemoryStorage()
 # bot = Bot(token=config.TOKEN, parse_mode=types.ParseMode.HTML, proxy=PROXY_URL) #, proxy=PROXY_URL — прокси
 bot = Bot(token=config.TOKEN, parse_mode=types.ParseMode.HTML)
